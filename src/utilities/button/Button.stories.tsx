9c5c9cc43858c2b35d9cf91181c9b866680d0754
Daniel Warren <78996771+dnlwrn01@users.noreply.github.com>
2022-06-17 11:19:36 -0500

    take number 4

--- src/utilities/button/Button.stories.tsx
+++ src/utilities/button/Button.stories.tsx
@@ -11,7 +11,7 @@ export default {
     background_color: { control: 'select' },
     background_color_density: { control: 'select' },
     padding_side: {control: 'multi-select'},
-    // test change x3
+    // test change x4
     //hide base inheritance 
     colors: {table: {disable: true}},
     hundreds: {table: {disable: true}},
