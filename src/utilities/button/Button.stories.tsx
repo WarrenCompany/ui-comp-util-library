ae4228e33f9ee02ee2325892bf09466a465f7ec0
Daniel Warren <78996771+dnlwrn01@users.noreply.github.com>
2022-06-17 11:16:11 -0500

    test local hook

--- src/utilities/button/Button.stories.tsx
+++ src/utilities/button/Button.stories.tsx
@@ -11,7 +11,7 @@ export default {
     background_color: { control: 'select' },
     background_color_density: { control: 'select' },
     padding_side: {control: 'multi-select'},
-
+    // test change
     //hide base inheritance 
     colors: {table: {disable: true}},
     hundreds: {table: {disable: true}},
